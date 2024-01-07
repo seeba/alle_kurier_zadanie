40cb88cb1b126ffe42a295b45ffd6602a7c5bb80
Sebastian Pluta <poczta@sebastianpluta.pl>
2024-01-07 01:11:34 +0100

    fix app:invoice:get-by-status-and-amount command

--- src/Core/Invoice/UserInterface/Cli/GetInvoices.php
+++ src/Core/Invoice/UserInterface/Cli/GetInvoices.php
@@ -5,6 +5,7 @@ namespace App\Core\Invoice\UserInterface\Cli;
 use App\Common\Bus\QueryBusInterface;
 use App\Core\Invoice\Application\DTO\InvoiceDTO;
 use App\Core\Invoice\Application\Query\GetInvoicesByStatusAndAmountGreater\GetInvoicesByStatusAndAmountGreaterQuery;
+use App\Core\Invoice\Domain\Status\InvoiceStatus;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -24,8 +25,11 @@ class GetInvoices extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+
         $invoices = $this->bus->dispatch(new GetInvoicesByStatusAndAmountGreaterQuery(
-            $input->getArgument('amount')
+            (int) $input->getArgument('amount'),
+            InvoiceStatus::from($input->getArgument('status'))
+            
         ));
 
         /** @var InvoiceDTO $invoice */
